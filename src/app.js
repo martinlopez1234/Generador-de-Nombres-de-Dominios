0cbc5919ce02e1153ebef4a2d7b6f03505f8894e
martinlopez1234 <martin.lr@hotmail.com>
2022-09-08 22:09:49 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var pronoun = ["the", "our"];
 var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
-var dominios = [".com", "net", ".us", ".io"];
+var dominios = [".com", ".net", ".us", ".io"];
 for (let i = 0; i <= 8; i++) {
   console.log(
     pronoun[Math.floor(Math.random() * pronoun.length)] +
