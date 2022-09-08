d0ad95515bbe6f58637870f7aa2e5c10e031e590
martinlopez1234 <martin.lr@hotmail.com>
2022-09-08 22:06:08 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var dominios = [".com", "net", ".us", ".io"];
+for (let i = 0; i <= 8; i++) {
+  console.log(
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+      adj[Math.floor(Math.random() * adj.length)] +
+      noun[Math.floor(Math.random() * noun.length)] +
+      dominios[Math.floor(Math.random() * dominios.length)]
+  );
+}
